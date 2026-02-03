93e095954433fbcc9d2a5552eb6d7dc7026fe095
Anasou06 <anas.baili@laplateforme.io>
2026-02-03 10:28:27 +0100

    start

---
+++ includes/footer.php
@@ -0,0 +1,8 @@
+<footer>
+
+<link href = "../css/footer.css" rel = "stylesheet">
+
+</footer>
+
+</body>
+</html>
---
+++ includes/header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>RDV avec Eipstein</title>
+    <link href="../css/accueil.css" rel="stylesheet">
+    <link href="../css/header.css" rel="stylesheet">
+    <link href = "../css/profile.css" rel = "stylesheet">
+</head>
+<body>
+
+<header>
+    <?php include "../includes/config.php"?>
+</header>
+<main>
