90bd5cc9c8e16fef5d7b5de05ccb7b2fbbc37fe1
Anasou06 <anas.baili@laplateforme.io>
2026-02-04 14:07:34 +0100

    amélioration accuiel

---
+++ php/accueil.php
@@ -0,0 +1,16 @@
+<?php include '../includes/header.php'; ?>
+
+<div class="page">
+<h1>Ceci est un site qui vous permets de prendre un rendez-vous avec Apoutchou Eipstein !</h1>
+<div class="box">
+    <a>
+        <img src = "../image/chica-rizz-rizz.gif" alt="GIF">
+    </a>
+    <a>
+        <img src = "../image/freddy-mewing.gif" alt="GIF">
+    </a>
+</div>
+</div>
+<?php include '../includes/footer.php'; ?>
+</body>
+</html>
